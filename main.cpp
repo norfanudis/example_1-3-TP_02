e18d6f95846d0e24af1aa4d90b4f280ea42294b1
NicoOrfa <norfanudis@gmail.com>
2023-09-08 00:06:27 +0000

    Cambiado a funciones de C++ a C

--- main.cpp
+++ main.cpp
@@ -3,14 +3,20 @@
 
 int main()
 {
-    DigitalIn gasDetector(D2);
-    DigitalIn overTempDetector(D3);
-    DigitalIn alarmOffButton(BUTTON1);
+    gpio_t gasDetector, overTempDetector, alarmOffButton, alarmLED;
+    //DigitalIn gasDetector(D2);
+    gpio_init_in_ex(&gasDetector, D2, PullDown);
+    gpio_init_in_ex(&overTempDetector, D3, PullDown);
+    gpio_init_in(&alarmOffButton, BUTTON1);
+    //DigitalIn gasDetector(D2);
+    //DigitalIn overTempDetector(D3);
+    //DigitalIn alarmOffButton(BUTTON1);
 
+    gpio_init_out(&alarmLED,LED1);
     DigitalOut alarmLed(LED1);
 
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
+    //gasDetector.mode(PullDown);
+    //overTempDetector.mode(PullDown);
 
     alarmLed = OFF;
 
@@ -18,13 +24,13 @@ int main()
 
     while (true) {
 
-        if ( gasDetector || overTempDetector ) {
+        if ( gpio_read(&gasDetector) || gpio_read(&overTempDetector) ) {
             alarmState = ON;
         }
 
         alarmLed = alarmState;
 
-        if ( alarmOffButton ) {
+        if ( gpio_read(&alarmOffButton) ) {
             alarmState = OFF;
         }
     }
