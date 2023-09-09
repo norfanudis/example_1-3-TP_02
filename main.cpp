ea9ef88d4f01b4799dfa587238f40c572b2d144f
norfanudis <norfanudis@fi.uba.ar>
2023-09-09 13:13:46 +0000

    Cambio de ciertas variables y funciones a Metodos "BusIn"

--- main.cpp
+++ main.cpp
@@ -4,19 +4,14 @@
 int main()
 {
     gpio_t gasDetector, overTempDetector, alarmOffButton, alarmLED;
-    //DigitalIn gasDetector(D2);
-    gpio_init_in_ex(&gasDetector, D2, PullDown);
-    gpio_init_in_ex(&overTempDetector, D3, PullDown);
-    gpio_init_in(&alarmOffButton, BUTTON1);
-    //DigitalIn gasDetector(D2);
-    //DigitalIn overTempDetector(D3);
-    //DigitalIn alarmOffButton(BUTTON1);
 
+    gpio_init_in_ex(&gasDetector, D2, PullDown);     //DigitalIn gasDetector(D2); gasDetector.mode(PullDown);
+    gpio_init_in_ex(&overTempDetector, D3, PullDown);//DigitalIn overTempDetector(D3); overTempDetector.mode(PullDown);
+    gpio_init_in(&alarmOffButton, BUTTON1);          //DigitalIn alarmOffButton(BUTTON1);
+    
     gpio_init_out(&alarmLED,LED1);
     DigitalOut alarmLed(LED1);
 
-    //gasDetector.mode(PullDown);
-    //overTempDetector.mode(PullDown);
 
     alarmLed = OFF;
 
@@ -25,7 +20,7 @@ int main()
     while (true) {
 
         if ( gpio_read(&gasDetector) || gpio_read(&overTempDetector) ) {
-            alarmState = ON;    //Comentario
+            alarmState = ON;
         }
 
         alarmLed = alarmState;
